9483935c4bf3f1134b5c4dcff9cbdb30142aeee6
Kristopher <kristopher_matic@hotmail.com>
2022-09-01 15:33:38 -0700

    Add form elements tie event listeners with api func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import getWeatherData from './weather';
 
-const weatherData = getWeatherData('london');
+const btn = document.querySelector('button');
+const input = document.querySelector('input');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+btn.addEventListener('click', async () => {
+    const data = await getWeatherData(input.value);
+    console.log(data);
+});
--- src/weather.js
+++ src/weather.js
@@ -19,13 +19,12 @@ async function getWeatherData(location) {
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=2856f7800e7e860f5f68bc404924d79f`;
         const response = await fetch(url, { mode: 'cors' });
         if (!response.ok) {
-            throw new Error('Error');
+            throw new Error('Location not found');
         }
         const data = cleanWeatherData(await response.json());
-        console.log(data);
         return data;
     } catch (error) {
-        console.log(error);
+        alert(error); // future: create less intrusive error
         return null;
     }
 }
